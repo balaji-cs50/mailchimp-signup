48342e267f4946357c6d647cadae0464de8d82bb
balaji-cs50 <84842681+balaji-cs50@users.noreply.github.com>
2023-06-19 11:31:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,13 +40,14 @@
 
     const request = https.request(url, options, function(response){
         response.on("data", function(data){
-            console.log(JSON.parse(data));
-            if(JSON.parse(data).error_count == 1){
+            if(response.statusCode === 200){
                 res.sendFile(__dirname + "/success.html");
             }
             else{
                 res.sendFile(__dirname + "/failure.html");
             }
+            console.log(JSON.parse(data));
+            
             
         })
     });
